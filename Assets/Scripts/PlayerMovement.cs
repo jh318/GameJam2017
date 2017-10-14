651de33be407ff62ad0896203b6aa86afab89048
BOB\Melisa <MAJohnson4683@muleriders.saumag.edu>
2017-10-13 22:04:52 -0500

    Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10.0F;
+    void Update()
+    {
+        float translation = Input.GetAxis("Horizontal") * speed;
+
+        translation *= Time.deltaTime;
+
+        transform.Translate(translation, 0, 0);
+
+    }
+}
+
