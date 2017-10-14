2a3a59c8c58781bb3b8745dcffc282c661420f14
BOB\Melisa <MAJohnson4683@muleriders.saumag.edu>
2017-10-14 07:18:36 -0500

    Fixed bullet prefab

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -3,18 +3,16 @@ using System.Collections;
 
 public class Shooting : MonoBehaviour{
 
-<<<<<<< HEAD
-   
-=======
+    public Rigidbody projectile;
     public float speed = 20;
->>>>>>> 86f4e43d821e08731cee4c547059ededd658ed11
+
 
     void Update()
     {
 
         if (Input.GetButtonDown("Fire1"))
         {
-<<<<<<< HEAD
+
             Rigidbody instantProjectile = Instantiate(projectile,
                                                            transform.position,
                                                            transform.rotation)
@@ -22,18 +20,19 @@ public class Shooting : MonoBehaviour{
 
             instantProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
 
-            Destroy(instantProjectile.gameObject, .5f);
-=======
+            
             GameObject bullet = Spawner.Spawn("Bullet");
             Rigidbody instantiatedProjectile = bullet.GetComponent<Rigidbody>();
             instantiatedProjectile.transform.position = transform.position;
             instantiatedProjectile.transform.rotation = transform.rotation;
 
             instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
->>>>>>> 86f4e43d821e08731cee4c547059ededd658ed11
+
+            
         }
 
-        
+
 
     }
+   
 }
