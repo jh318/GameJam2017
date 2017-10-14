aade679d0d2ce92cbde38e2546de142d7933f461
John Hawley <jhawley318@gmail.com>
2017-10-14 16:29:12 -0500

    fixed mario texture. ghosts spawn with random color

---
+++ Assets/GhostColor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostColor : MonoBehaviour {
+
+
+
+	void Start(){
+		GetComponentInChildren<Renderer>().material.color = new Color(Random.Range(0.0f,2.0f), Random.Range(0.0f,2.0f),Random.Range(0.0f,2.0f),Random.Range(0.0f,2.0f));
+	}
+}
