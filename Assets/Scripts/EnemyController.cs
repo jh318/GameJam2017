52abf30bca873de76c0ef7c8d9ec0a110d098e2c
John Hawley <jhawley318@gmail.com>
2017-10-13 22:19:40 -0500

    implemented basic enemycontroller, enemy moves toward player, coroutines for moving sideways randomly, need to fix bug not letting it move too the left. clamped to some x range

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,13 +4,49 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
 
-	public float speed = 1.0f;
+	public float forwardSpeed = 1.0f;
+	public float sideSpeed = 1.0f;
+	public float sideTime = 1.0f;
+	public float randomNumberRollSeconds = 3.0f;
+	public float leftClamp = -10.0f;
+	public float rightClamp = 10.0f;
+
+	int randomNumber = 0;
 
 	void Start(){
 		Transform transform = GetComponent<Transform>();
+		StartCoroutine("RandomNumberGenerator", randomNumberRollSeconds);
 	}
 
 	void Update(){
-		transform.position += Vector3.forward * -speed;
+		transform.position += Vector3.forward * -forwardSpeed;
+
+		if(randomNumber == 1){
+			StopCoroutine("SwitchLane");			
+			StartCoroutine("SwitchLane", sideTime);
+		}
+		else if(randomNumber == 2){
+			sideSpeed = sideSpeed * -1;
+			StopCoroutine("SwitchLane");
+			StartCoroutine("SwitchLane", sideTime);	
+		}
+
+        transform.position = new Vector3(Mathf.Clamp(Time.time, leftClamp, rightClamp), transform.position.y, transform.position.z);
+	}
+
+	IEnumerator SwitchLane(float time){
+		while(time > 0.0f){
+			transform.position += Vector3.right * sideSpeed;
+			time -= Time.deltaTime;
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+	IEnumerator RandomNumberGenerator(float time){
+		while(true){
+			yield return new WaitForSeconds(time);
+			randomNumber = Random.Range(0, 3);
+			Debug.Log(randomNumber);
+		}
 	}
 }
