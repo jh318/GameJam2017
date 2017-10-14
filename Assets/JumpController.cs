252456caff31ea42984ac93f11e91c148a5e52ed
John Hawley <jhawley318@gmail.com>
2017-10-14 12:51:48 -0500

    made jumpman jump correctly. scaled gravity up in physics. adjusted plane box collider. added box colliders to enemies.

--- Assets/JumpController.cs
+++ Assets/JumpController.cs
@@ -4,49 +4,26 @@ using UnityEngine;
 
 public class JumpController : MonoBehaviour {
 
-	public float jumpHeight = 5.0f;
-	public float jumpSpeed = 1.0f;
-	public float jumpTime = 2.0f;
+	public float jumpForce = 1000.0f;
 	public float jumpInterval = 2.0f;
-	public float jumpIntervalChangeTime = 5.0f;
 
 	float randomNumber = 0.0f;
 	Rigidbody body;
 
 	void Start(){
-		body = GetComponent<Rigidbody>();		
-		//StartCoroutine("Jump", jumpTime);
-		//StartCoroutine("RandomlySetJumpInterval", jumpIntervalChangeTime);
-		//StartCoroutine("JumpLoop");
-	//	JumpBody();
-		//StartCoroutine("Wait");
+		body = GetComponent<Rigidbody>();
+		StartCoroutine("JumpLoop");
+	}	
+
+	void Jump(){
+		body.AddForce(transform.up * jumpForce);
 	}
 
-	void Update(){
-		
-	}
-
-
-	IEnumerator Jump(){
-		while(jumpTime > 0.0f){
-			transform.position += Vector3.up * jumpSpeed;
-			jumpTime -= Time.deltaTime;
-			yield return new WaitForEndOfFrame();
-		}
-
-		yield return StartCoroutine("Fall");	
-	}
-
-	void JumpBody(){
-		body.AddForce(transform.up * 100);
-	}
-
-	IEnumerator Fall(){
-		while(jumpTime > 0.0f){
-			transform.position += Vector3.down * jumpSpeed;
-			jumpTime -= Time.deltaTime;
-			yield return new WaitForEndOfFrame();
-		}
+	IEnumerator JumpLoop(){
+		while(true){
+			yield return new WaitForSeconds(jumpInterval);
+			Jump();
+		}	
 	}
 
 	IEnumerator RandomlySetJumpInterval(float time){
@@ -54,17 +31,5 @@ public class JumpController : MonoBehaviour {
 			yield return new WaitForSeconds(time);
 			jumpInterval = Random.Range(0.5f, 5.0f);
 		}
-	}
-
-	IEnumerator JumpLoop(){
-		while(true){
-			yield return new WaitForSeconds(jumpInterval);
-			yield return StartCoroutine("Jump");
-		}	
-	}
-
-	IEnumerator Wait(){
-		yield return new WaitForSeconds(2.0f);
-		JumpBody();
 	}
 }
