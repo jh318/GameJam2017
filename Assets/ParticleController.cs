e1223919861756c7eb98ed6b4ad64e0dddf01398
John Hawley <jhawley318@gmail.com>
2017-10-15 12:59:49 -0500

    set up final stuff and polish. game is published to digitech. good tot go. sort of rare issue with ship getting knocked under grid but still plays fine. Should be ready for arcade machine too

---
+++ Assets/ParticleController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour {
+
+	void OnEnable(){
+		StartCoroutine("SetInactive");
+	}
+
+	IEnumerator SetInactive(){
+		yield return new WaitForSeconds(1.5f);
+		gameObject.SetActive(false);
+	}
+}
